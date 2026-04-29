ac256f130d92edb065bf0cf08b82dd2cce05e09a
cahayoyo <cahayoyo@gmail.com>
2026-04-29 18:39:29 +0700

    add TC007,0023,0024

--- src/test/java/tests/DropDownTests.java
+++ src/test/java/tests/DropDownTests.java
@@ -4,7 +4,10 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.DropDown;
+import pages.HistoryPage;
 import pages.HomePage;
+import pages.LoginPage;
+import pages.ProfilePage;
 import utils.Config;
 import utils.Helper;
 import utils.Log;
@@ -46,7 +49,6 @@ public class DropDownTests extends BaseTest {
 	public void TC006_OpenCURAHomeViaHomeButton() {
 		Log.info("=== Running: TC006_Open CURA Home via Home button ===");
 
-		Log.info("Step: Navigate to Login Page first.");
 
 		HomePage homePage = new HomePage(driver);
 		DropDown dropDown = new DropDown(driver);
@@ -71,6 +73,96 @@ public class DropDownTests extends BaseTest {
 		Helper.verifyElementEqualsText(homePage.getH1(), "CURA Healthcare Service", "Header H1 after redirection");
 
 		Log.info("=== TC006 Finished ===");
+
+	}
+	
+	@Test
+	public void TC007_OpenLoginViaLoginButton() {
+		Log.info("=== Running: TC007_Open Login via Login button ===");
+
+		DropDown dropDown = new DropDown(driver);
+		LoginPage login = new LoginPage(driver);
+
+		try {
+			dropDown.clickMenuToggle();
+			Log.info("Step: Clicked Menu Toggle.");
+
+			Thread.sleep(500);
+
+			dropDown.clickLinkLogin();
+			Log.info("Step: Clicked Login button.");
+		} catch (Exception e) {
+			Log.error("Retrying: Clicking login button directly...");
+			dropDown.clickMenuToggle();
+			dropDown.clickLinkLogin();
+		}
+
+		String expectedHomeUrl = Config.BASE_URL_LOGIN;
+		Helper.verifyEqualsUrl(driver, expectedHomeUrl, "Login via Login button Sidebar");
+
+		Helper.verifyElementEqualsText(login.geth2Login(), "Login", "Header H2 Login");
+
+		Log.info("=== TC007 Finished ===");
+
+	}
+	
+	@Test
+	public void TC023_OpenHistoryViaHistoryButton() {
+		Log.info("=== Running: TC023_Open History via History button ===");
+
+		DropDown dropDown = new DropDown(driver);
+		HistoryPage history = new HistoryPage(driver); 
+
+		try {
+			dropDown.clickMenuToggle();
+			Log.info("Step: Clicked Menu Toggle.");
+
+			Thread.sleep(500);
+
+			dropDown.clickLinkHistory();
+			Log.info("Step: Clicked History button.");
+		} catch (Exception e) {
+			Log.error("Retrying: Clicking history button directly...");
+			dropDown.clickMenuToggle();
+			dropDown.clickLinkHistory();
+		}
+
+		String expectedHomeUrl = Config.BASE_URL_HISTORY;
+		Helper.verifyEqualsUrl(driver, expectedHomeUrl, "History via History button");
+
+		Helper.verifyElementEqualsText(history.getH2History(), "History", "Header H2 History");
+
+		Log.info("=== TC023 Finished ===");
+
+	}
+	
+	@Test
+	public void TC024_OpenProfileViaProfileButton() {
+		Log.info("=== Running: TC024_Open Profile via Profile button ===");
+
+		DropDown dropDown = new DropDown(driver);
+		ProfilePage profile = new ProfilePage(driver); 
+
+		try {
+			dropDown.clickMenuToggle();
+			Log.info("Step: Clicked Menu Toggle.");
+
+			Thread.sleep(500);
+
+			dropDown.clickLinkProfile();
+			Log.info("Step: Clicked Profile button.");
+		} catch (Exception e) {
+			Log.error("Retrying: Clicking profile button directly...");
+			dropDown.clickMenuToggle();
+			dropDown.clickLinkProfile();
+		}
+
+		String expectedHomeUrl = Config.BASE_URL_PROFILE;
+		Helper.verifyEqualsUrl(driver, expectedHomeUrl, "Profile via Profile button");
+
+		Helper.verifyElementEqualsText(profile.getH2Profile(), "Profile", "Header H2 Profile");
+
+		Log.info("=== TC024 Finished ===");
 
 	}
 }
