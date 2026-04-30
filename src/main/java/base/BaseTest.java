b60f541c51d1d360bae5852764fa77c8d5c2accc
cahayoyo <cahayoyo@gmail.com>
2026-04-30 12:05:21 +0700

    add extentreport

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,19 +1,36 @@
 package base;
 
+import java.lang.reflect.Method;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 import utils.Log;
 
 import utils.Config;
+import utils.ExtentReportManager;
 
 public class BaseTest {
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setUpReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
 	
 	@BeforeMethod
-	public void setUp() {
+	public void setUp(Method method) {
 		Log.info("=== Starting WebDriver ===");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -23,11 +40,24 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		if(result.getStatus() == ITestResult.FAILURE) {
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "DEFECT");
+			test.fail("TC Failed - Check Screenshot", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}else if(result.getStatus() == ITestResult.SUCCESS) {
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "PASSED");
+			test.info("TC Failed - Check Screenshot", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+		
 		if(driver != null) {
 			driver.quit();
 			Log.info("=== Browser Closed ===");
 		}
+	}
+	
+	@AfterSuite
+	public void tearDownReport() {
+		extent.flush();
 	}
 
 }
--- src/test/java/tests/HomeTests.java
+++ src/test/java/tests/HomeTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.HomePage;
+import utils.ExtentReportManager;
 import utils.Helper;
 import utils.Log;
 
@@ -12,32 +13,47 @@ public class HomeTests extends BaseTest {
 	@Test
     public void TC001_VerifyHomePage() {
         Log.info("=== Running: TC001_VerifyHomePage ===");
+        test = ExtentReportManager.createTest("TC001_VerifyHomePage");
         
         HomePage home = new HomePage(driver);
 
         Helper.verifyElementEqualsText(home.getH1(), "CURA Healthcare Service", "Header H1");
+        test.info("Verifying Header H1");
         Helper.verifyElementEqualsText(home.getH3(), "We Care About Your Health", "Header H3");
+        test.info("Verifying Header H3");
 
         Helper.verifyElementEqualsText(home.getBtnMakeAppointment(), "Make Appointment", "Button Make Appointment");
+        test.info("Verifying Button Make Appointment");
 
         Helper.verifyElementEqualsText(home.getFooterTitle(), "CURA Healthcare Service", "Footer Title");
+        test.info("Verifying Footer CURA");
 
         Helper.verifyElementContainsText(home.getFooterAddress(), "Atlanta 550 Pharr Road NE Suite 525", "Footer Address");
+        test.info("Verifying Footer Address");
         Helper.verifyElementContainsText(home.getFooterPhone(), "(678) 813-1KMS", "Footer Phone");
+        test.info("Verifying Footer Phone");
         Helper.verifyElementEqualsText(home.getFooterEmail(), "info@katalon.com", "Footer Email");
+        test.info("Verifying Footer Email");
 
         Helper.verifyElementDisplayed(home.getFbIcon(), "Icon Facebook");
-        Helper.verifyElementDisplayed(home.getTwitterIcon(), "Icon Twitter");
+        test.info("Verifying Icon Facebook");
+        Helper.verifyElementDisplayed(home.getTwitterIcon(), "Icon X");
+        test.info("Verifying Icon X");
         Helper.verifyElementDisplayed(home.getDribbbleIcon(), "Icon Dribbble");
+        test.info("Verifying Icon Dribble");
 
         Helper.verifyElementContainsText(home.getFooterCopyright(), "CURA Healthcare Service 2026", "Footer Copyright");
+        test.info("Verifying Footer Copyright");
 
         Log.info("=== TC001 Finished ===");
+        test.pass("TC001 PASSED");
+
     }
     
 	@Test
     public void TC002_OpeningCuraFacebook() {
 		Log.info("=== Running: TC002_OpeningCuraFacebook ===");
+		test = ExtentReportManager.createTest("TC002_OpeningCuraFacebook");
         
         HomePage home = new HomePage(driver);
         home.getFbIcon().click();
@@ -50,6 +66,7 @@ public class HomeTests extends BaseTest {
 	@Test
     public void TC003_OpeningCuraTwitter() {
 		Log.info("=== Running: TC003_OpeningCuraTwitter ===");
+		test = ExtentReportManager.createTest("TC003_OpeningCuraTwitter");
         
         HomePage home = new HomePage(driver);
         home.getTwitterIcon().click();
@@ -62,6 +79,7 @@ public class HomeTests extends BaseTest {
 	@Test
     public void TC004_OpeningCuraDribbble() {
 		Log.info("=== Running: TC004_OpeningCuraDribbble ===");
+		test = ExtentReportManager.createTest("TC004_OpeningCuraDribbble");
         
         HomePage home = new HomePage(driver);
         home.getDribbbleIcon().click();
